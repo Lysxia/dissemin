fc4a83d82f0a658e21b263e5a561a52e001245ce
lyxia <li-yao.xia@ens.fr>
2016-07-04 00:23:26 +0200

    Fix

--- publishers/search_indexes.py
+++ publishers/search_indexes.py
@@ -4,12 +4,9 @@ from haystack import indexes
 
 class PublisherIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    num_papers = indexes.IntegerField(model_attr='stats__num_tot')
+    num_papers = indexes.IntegerField(model_attr='stats__num_tot', default=0)
     oa_status = indexes.CharField(model_attr='oa_status')
     name = indexes.CharField(model_attr='name')
 
     def get_model(self):
         return Publisher
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.filter(stats__isnull=False)
